65a5b89c246e30dda0590be94bbcbf35bcf6cfcd
Spencer Haney <Spencer Haney>
2016-09-04 13:53:18 -0400

    Added reimagined stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float speed;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(x, y) * speed;
+	}
+}
