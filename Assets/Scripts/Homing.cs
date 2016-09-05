9f6af2424e06ae79fa8871eb62d8e2d65086342a
Spencer Haney <theOfficialSkara@gmail.com>
2016-09-05 02:09:08 -0400

    Remade as 2D project

---
+++ Assets/Scripts/Homing.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class Homing : MonoBehaviour
+{
+	public Transform target;
+
+	[SerializeField]
+	private float speed;
+
+	private Rigidbody2D rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		if (target == null)
+		{
+			return;
+		}
+
+		Vector2 pos = Util.convert (transform.position);
+		Vector2 targetPos = Util.convert (target.position);
+		Vector2 neededForce = targetPos - pos - rb.velocity;
+
+		Debug.Log (neededForce * speed);
+
+		if (Util.isZero (neededForce))
+		{
+			Debug.Log ("Exiting");
+			return;
+		}
+
+		rb.AddForce (neededForce * speed);
+	}
+}
---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util
+{
+	public static Vector2 convert (Vector3 vec)
+	{
+		return new Vector2 (vec.x, vec.y);
+	}
+
+	public static bool isZero (Vector2 vec)
+	{
+		return Mathf.Approximately (vec.x, 0.0f) && Mathf.Approximately (vec.y, 0.0f);
+	}
+}
